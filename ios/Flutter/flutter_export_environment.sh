80b69ea20a6239bfdcfcbd985218b08db47009bd
LudoO <ludovic.valente@gmail.com>
2019-12-26 23:12:24 +0100

    add firebase lib

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/ludoo/dev/flutter/flutter_sdk"
-export "FLUTTER_APPLICATION_PATH=/Users/ludoo/dev/flutter/_refactoring/code4func_flutter-taxiapp"
+export "FLUTTER_APPLICATION_PATH=/Users/ludoo/dev/flutter/_refactoring/code4func_flutter-taxiapp/tmp_upgrade/fl_uberapp"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
